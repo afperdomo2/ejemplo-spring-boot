44089e1493ca760cff09f2ed1889bef8c842700a
alvarado0925 <alvarado0925@gmail.com>
2023-12-06 23:12:17 -0500

    First commit

---
+++ src/main/java/com/felipe/Ejemplo_2023/Ejemplo2023Application.java
@@ -0,0 +1,13 @@
+package com.felipe.Ejemplo_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/Ejemplo_2023/Ejemplo2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.Ejemplo_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
